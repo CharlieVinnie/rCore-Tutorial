5eb8ca14ee8b1aef6662cd40dcc98b33daf7659f
Charlie <charliewang.cpp@gmail.com>
2026-02-19 19:16:48 +0800

    merge sys_get_time

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -1,10 +1,10 @@
 use crate::{
     fs::{open_file, OpenFlags},
-    mm::{translated_ref, translated_refmut, translated_str},
+    mm::{translated_ref, translated_refmut, translated_str, translated_byte_buffer},
     task::{
         current_process, current_task, current_user_token, exit_current_and_run_next, pid2process,
         suspend_current_and_run_next, SignalFlags,
-    },
+    }, timer::get_time_us,
 };
 use alloc::{string::String, sync::Arc, vec::Vec};
 
@@ -151,12 +151,25 @@ pub fn sys_kill(pid: usize, signal: u32) -> isize {
 /// YOUR JOB: get time with second and microsecond
 /// HINT: You might reimplement it with virtual memory management.
 /// HINT: What if [`TimeVal`] is splitted by two pages ?
-pub fn sys_get_time(_ts: *mut TimeVal, _tz: usize) -> isize {
-    trace!(
-        "kernel:pid[{}] sys_get_time NOT IMPLEMENTED",
-        current_task().unwrap().process.upgrade().unwrap().getpid()
-    );
-    -1
+pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
+    trace!("kernel: sys_get_time");
+    let time_us = get_time_us();
+    let time = TimeVal {
+        sec: time_us / 1_000_000,
+        usec: time_us % 1_000_000,
+    };
+    let current_token = current_user_token();
+    let buffer = translated_byte_buffer(current_token, ts as *const u8, core::mem::size_of::<TimeVal>());
+    let src = unsafe {
+        core::slice::from_raw_parts(&time as *const TimeVal as *const u8, core::mem::size_of::<TimeVal>())
+    };
+    let mut offset = 0;
+    for dst in buffer {
+        let len = dst.len();
+        dst.copy_from_slice(&src[offset..offset+len]);
+        offset += len;
+    }
+    0
 }
 
 /// mmap syscall
